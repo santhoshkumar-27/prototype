146ce85038abf305e23ce36b4f66c648a1b8586b
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-10-21 10:21:10 +0530

    changes in the prototype chain and chain

--- index.js
+++ index.js
@@ -14,15 +14,17 @@ function studentClass(n, a) {
 //         this.age = a;
 //     }
 // }
+// if we miss any properties in declaration, we can use a prototype object we can declare
 const s1 = new studentClass('santhoshkumar', 24);
 const s2 = new studentClass('sandy', 27);
 // for prototype purpose
 // default every function has prototype object so that we give new properties and method
 // we can give us a value or a function 
 // studentClass.prototype.year = '1st year';
-studentClass.prototype.year = function() {
+studentClass.prototype.year = function() { // its a anonoyms function
     return '1st year'
 };
 
 // console.log(s1.year);
 console.log(s1.year());
+//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes#:~:text=Every%20object%20in%20JavaScript%20has,null%20for%20its%20own%20prototype.
